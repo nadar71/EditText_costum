c1063103b42908db8e04739fa974ca19f2018af7
Simone <simone.mapelli@gmail.com>
2020-04-07 19:32:39 +0200

    added new shape, rename other resources

--- app/src/main/java/com/mapelli/simone/edittextcostum/MainActivity.kt
+++ app/src/main/java/com/mapelli/simone/edittextcostum/MainActivity.kt
@@ -2,11 +2,18 @@ package com.mapelli.simone.edittextcostum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val m_cube_text = findViewById<TextView>(R.id.m_cube_text)
+
+        m_cube_text.text = Html.fromHtml("m<sup>3</sup>")
+
     }
 }
